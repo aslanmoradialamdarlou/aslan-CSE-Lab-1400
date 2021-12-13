576961df59583375b2147af9966698befdd63ba7
aslan moradi <aslanmoradialamdarlu81@gmail.com>
2021-12-13 09:27:19 +0330

    python file upgraded

--- is_prime.py
+++ is_prime.py
@@ -1,11 +1,16 @@
 n = int(input())
 a = n//2
-flag = True
-for i in range(2,a+1):
-    if n % i == 0 :
-        flag = False
-	break
-if flag :
+if n > 2:
+    flag = True
+    for i in range(2,a+1):
+        if n % i == 0 :
+            flag = False
+	    break
+    if flag :
+        print('Yes')
+    else:
+        print('No')
+elif n == 2:
     print('Yes')
-else:
-    print('No')
+elif n == 1:
+    print('we have no answer to this input')
